ceafd551c59f001c48a42e6608d772b0cd552358
root <root@9ce9c5475ab2>
2019-06-24 14:57:21 +0000

    final for TTS lazy CD

--- scripts/build-tts-flask.sh
+++ scripts/build-tts-flask.sh
@@ -1,6 +1,13 @@
 #Example script
-mkdir tmp 
-cp ../tts-flask/matches.csv ./tmp/matches.csv
-cp ../tts-flask/players ./tmp/players
-cp ./tts-flask ../
+mkdir tmp
+cd ./tts-flask
+rm -f players
+rm -f players.csv
+set +e
+cp  ../tts-flask/matches.csv ./tmp/matches.csv
+cp -r -f ../tts-flask/players/* ./tmp/players/
+rm -r -f ../tts-flask
+cp -r ./tts-flask /home/main/tts-flask
+cp -r -f ./tmp/players/* ../tts-flask/players
+cp -r -f ./tmp/matches.csv ../tts-flask/matches.csv
 supervisorctl restart tts
